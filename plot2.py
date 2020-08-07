c22ef4161ccbf0acc9a892e39c678a3f20038625
amiringit <ahkhoshbooie@mun.ca>
2020-08-07 17:31:53 -0230

    plot2

--- plot2.py
+++ plot2.py
@@ -1,6 +1,9 @@
+from matplotlib import pyplot as plt
+from countflights import *
+
 #to make second figure
 data["sum"] = data.sum(axis=1)
 plt.figure(figsize=(12,12))
-plt.plot(data.index, data["sum"])
+plt.plot(data["sum"])
 plt.savefig('plot2.png')
 
